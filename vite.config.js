cf08a2f0177a8860ad9f9616607de21c8a48e6ba
FabioSantos99 <120610757+FabioSantos99@users.noreply.github.com>
2023-09-30 01:44:33 -0300

    configurações

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
-// vite.config.js
-export default {
-    // config options
-  }
+import { defineConfig } from "vite";
+
+
+export default defineConfig({
+  plugins: [vanilla()],
+  base: "/cellphone_sa",
+});
